dcfc5fa6ba19d512eb224c5ca3c6fca9e99c2cd7
gyfweusiok <csjchrishinings@hotmail.com>
2024-05-14 07:54:12 +0700

    demo

--- index.js
+++ index.js
@@ -1,15 +1,10 @@
-const binarySearchIterative = (arr, target) => {
-  let left = 0;
-  let right = arr.length - 1;
-  while (left <= right) {
-    const mid = Math.floor((left + right) / 2);
-    if (arr[mid] === target) {
-      return mid;
-    } else if (arr[mid] < target) {
-      left = mid + 1;
-    } else {
-      right = mid - 1;
-    }
+function rob(nums) {
+  let prevMax = 0;
+  let currMax = 0;
+  for (const num of nums) {
+    const temp = currMax;
+    currMax = Math.max(prevMax + num, currMax);
+    prevMax = temp;
   }
-  return -1;
-};
+  return currMax;
+}
